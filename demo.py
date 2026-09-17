b8b38fca8c5d9021f79161e3f6b5a268a411dea4
imcode <128029980+ilyas11m@users.noreply.github.com>
2026-09-17 11:15:43 +0300

    sqush commit

--- demo.py
+++ demo.py
@@ -2,4 +2,13 @@ def test_func() -> None:
     print("Hello! and Goodbye!")
     print("How are you?")
 
+def demo_func() -> None:
+    print("Hi!")
+
+def two_sum() -> None:
+    print(5 + 6)
+
+def ping() -> None:
+    print("pong")
+
 test_func()
